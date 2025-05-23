6ad1340f6e082380114d0cfe1a03d8034c20c04b
temuriee <critstoggle@gmail.com>
2025-05-23 21:45:28 +0400

    w

---
+++ script.js
@@ -0,0 +1,62 @@
+// let myList = [5, 10, "Hello"];
+
+// let myObj = {
+//   id: 10,
+// };
+
+// let myObj2 = {
+//   id: 10,
+// };
+
+// let numb1 = 10;
+// let numb2 = 10;
+
+// console.log(numb1 === numb2);
+
+// console.log(myObj.id === myObj2.id);
+
+// console.log(myList);
+// console.log(myObj);
+
+// mizani : gamarjobis tqma
+
+// function sayHello() {
+//   alert(`"Hello World"`);
+// }
+
+// sayHello();
+
+// function sayHelloToUser() {
+//   let userName = prompt("შეიყვანეთ თქვენი სახელი");
+
+//   console.log(`Hello ${userName}`);
+// }
+
+// sayHelloToUser();
+
+// function squareNumberOne() {
+//   let numb1 = 10;
+//   let squareNumber = numb1 * numb1;
+
+//   return squareNumber;
+// }
+
+// function squareNumberTwo() {
+//   let numb1 = 20;
+
+//   return numb1 * numb1;
+// }
+
+// console.log(squareNumberOne() > squareNumberTwo());
+
+// function plusNumber(numberOne, numberTwo) {
+//   return numberOne + numberTwo;
+// }
+
+// console.log(plusNumber(1000, 20));
+// console.log(plusNumber(60, 40));
+
+// mizani: dagvibrunos 5 priovnebis sashvalo qula
+function avarageNumber() {}
+
+avarageNumber(4, 5, 1, 0, 10); // return : avarage
